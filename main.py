0c716fc145ab98fe4380d22b00c96f02b44f07a0
3119005485张洪 <1220410556@qq.com>
2021-09-17 23:02:47 +0800

    第二次提交

--- main.py
+++ main.py
@@ -1,88 +1,95 @@
 #encoding=utf8
 
-import tensorflow as tf 
-import keras 
+import tensorflow as tf
 import numpy as np
 import sys
 import csv
 sys.path.append('../')
 from drcn import DRCN
-from load_data import load_char_data,load_word_embed,load_char_embed,load_all_data
+from load_data import load_word_embed,load_char_embed,load_all_data
+from line_profiler_pycharm import profile
 
 
 np.random.seed(1)
 tf.set_random_seed(1)
 
-params = {
-    'num_classes':2,
-    'max_features':1700,
-    'embed_size':100,
-    'filters':300,
-    'kernel_size':3,
-    'strides':1,
-    'padding':'same',
-    'conv_activation_func':'relu',
-    'embedding_matrix':[],
-    'w_initializer':'random_uniform',
-    'b_initializer':'zeros',
-    'dropout_rate':0.5,
-    'mlp_activation_func':'relu',
-    'mlp_num_layers':1,
-    'mlp_num_units':256,
-    'mlp_num_fan_out':128,
-    'task':'Classification',
-    'input_shapes':[(48,),(48,),(48,),(48,)],
-    'lstm_units':64,
-    'num_blocks':1,
-    'word_max_features':7300,
-    'word_embed_size':100,
-}
+@profile
+def yuce():
 
-if __name__ == '__main__':
+    base_params = {
+        'num_classes':2,
+        'max_features':1700,
+        'embed_size':100,
+        'filters':300,
+        'kernel_size':3,
+        'strides':1,
+        'padding':'same',
+        'conv_activation_func':'relu',
+        'embedding_matrix':[],
+        'w_initializer':'random_uniform',
+        'b_initializer':'zeros',
+        'dropout_rate':0.2,
+        'mlp_activation_func':'relu',
+        'mlp_num_layers':1,
+        'mlp_num_units':256,
+        'mlp_num_fan_out':128,
+        'input_shapes':[(48,),(48,),(48,),(48,)],
+        'task':'Classification',
+        'lstm_units':64,
+        'num_blocks':1,
+        'word_max_features':7300,
+        'word_embed_size':100
+    }
 
     org_text_path = sys.argv[1]
     org_change_path = sys.argv[2]
     ans_text_path = sys.argv[3]
-    org_text = open(org_text_path, "r",encoding='utf-8')
+    org_text = open(org_text_path, "r", encoding='utf-8')
     org = org_text.read()
-    org_change = open(org_change_path, "r",encoding='utf-8')
-    add = org_change.read()
-    with open('zhanghong.csv', "w",encoding='gbk') as work_path:
+    org_change = open(org_change_path, "r", encoding='utf-8')
+    change = org_change.read()
+    with open('zhanghong.csv', "w", encoding='gbk') as work_path:
         header = ['org', 'org_change', 'similarity']
         writer = csv.DictWriter(work_path, fieldnames=header)
         writer.writeheader()
-        writer.writerow({'org': org, 'org_change': add, 'similarity': 1})
+        writer.writerow({'org': org, 'org_change': change, 'similarity': 1})
 
-    word_embedding_matrix = load_word_embed(params['word_max_features'], params['word_embed_size'])
-    char_embedding_matrix = load_char_embed(params['max_features'], params['embed_size'])
+    word_embedding_matrix = load_word_embed(base_params['word_max_features'], base_params['word_embed_size'])
+    char_embedding_matrix = load_char_embed(base_params['max_features'], base_params['embed_size'])
 
-    params['embedding_matrix'] = char_embedding_matrix
-    params['word_embedding_matrix'] = word_embedding_matrix
-    params['embedding_matrix'] = char_embedding_matrix
+    base_params['embedding_matrix'] = char_embedding_matrix
+    base_params['word_embedding_matrix'] = word_embedding_matrix
 
-    backend = DRCN(params)
+    backend = DRCN(base_params)
 
-    p_c_index_test, h_c_index_test, p_w_index_test, h_w_index_test, same_word_test, y_test = load_all_data(
-        'zhanghong.csv', maxlen=params['input_shapes'][0][0])
+    p_c_index_test, h_c_index_test, p_w_index_test, h_w_index_test, same_word_test, _ = load_all_data(
+        'zhanghong.csv', maxlen=base_params['input_shapes'][0][0])
     x_test = [p_c_index_test, h_c_index_test, p_w_index_test, h_w_index_test]
-    y_test = keras.utils.to_categorical(y_test, num_classes=params['num_classes'])
 
     model = backend.build()
+
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
 
     bast_model_filepath = 'best_drcn_model.h5'
 
     model.load_weights(bast_model_filepath)
 
-    model.compile(
-        loss='categorical_crossentropy', 
-        optimizer='adam', 
-        metrics=['accuracy']
-        )
-
-    similarity = model.predict(
+    end = model.predict(
         x=x_test,
-        )
+    )
+
+    print(end[0][1])
 
     with open(ans_text_path, "w") as ans_text:
-        for tag in similarity:
-            ans_text.write(str(tag[1]))
+        if len(change) == 0:
+            ans_text.write(str(0))
+        else:
+            ans_text.write(str(end[0][1]))
+
+if __name__ == '__main__':
+
+    yuce()
---
+++ test.py
@@ -0,0 +1,17 @@
+from main import yuce
+
+if __name__ == '__main__':
+
+    yuce('org.txt','test_1.txt','test_ans_1.txt')
+    yuce('org.txt','test_2.txt','test_ans_2.txt')
+    yuce('org.txt','test_3.txt','test_ans_3.txt')
+    yuce('org.txt','test_4.txt','test_ans_4.txt')
+    yuce('org.txt','test_5.txt','test_ans_5.txt')
+    yuce('org.txt','test_6.txt','test_ans_6.txt')
+    yuce('org.txt','test_7.txt','test_ans_7.txt')
+    yuce('org.txt','test_8.txt','test_ans_8.txt')
+    yuce('org.txt','test_9.txt','test_ans_9.txt')
+    yuce('org.txt','test_10.txt','test_ans_10.txt')
+    yuce('org.txt','test_11.txt','test_ans_11.txt')
+    yuce('org.txt','test_12.txt','test_ans_12.txt')
+    yuce('org.txt','test_13.txt','test_ans_13.txt')
